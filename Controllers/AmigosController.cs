f1fc47c3106ea4cd4583031c466c68f2970e216c
Maverickk63 <103067766+Maverickk63@users.noreply.github.com>
2025-05-18 20:59:46 -0300

    Codigo Exemplo 0.8

---
+++ Controllers/AmigosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ViniAPI_1_Exemplo.Model;
+
+namespace ViniAPI_1_Exemplo.Controllers
+{
+    [Route("api/amigos")]
+
+    [ApiController]
+    public class AmigosController : Controller
+    {
+        //private static List<Amigo> ListaAmigos = new List<Amigo>()
+        private static List<Amigo> ListaAmigos = [
+                new Amigo(1,"Vinicius","Rosalen", 28, "PS5"),
+                new Amigo(2,"Isis","Rosalen", 8, "Gabarito da Prova"),
+                new Amigo(3,"Esis","Rosalen", 2, "Pontos Extras")
+            ];
+
+        public IEnumerable<Amigo> Get()
+        {
+            return ListaAmigos;
+        }
+        // GET: AmigosController
+
+        [HttpGet("{id}")]
+        public Amigo Get(int id)
+        {
+            Amigo? amg = ListaAmigos.Find(x => x.Id == id);
+
+            return amg ?? new();
+        }
+
+        [HttpGet("{id}/{nome}")]
+
+        public Amigo Get(int id, string nome)
+        {
+            Console.WriteLine($"id fake: {id}");
+            Amigo? amg = ListaAmigos.Find(x=> x.PrimeiroNome == nome);
+            return amg ?? new();
+        }
+    }
+}
---
+++ Model/Amigo.cs
@@ -0,0 +1,25 @@
+﻿namespace ViniAPI_1_Exemplo.Model
+{
+    public class Amigo
+    {
+        public int Id { get; set; }
+        public string? PrimeiroNome { get; set; }
+        public string? SobreNome { get; set; }
+        public int Idade { get; set; }
+        public string? OqueEmprestei { get; set; }
+
+        public Amigo()
+        {
+
+        }
+
+        public Amigo(int id ,string primeiroNome, string sobreNome, int idade, string oqueEmprestei)
+        {
+            Id = id;
+            PrimeiroNome = primeiroNome;
+            SobreNome = sobreNome;
+            Idade = idade;
+            OqueEmprestei = oqueEmprestei;
+        }
+    }
+}
